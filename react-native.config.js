b31049a41032f9af36e7e3fa156930f13f42466f
Mitch Silva <mitchelsonps@gmail.com>
2025-11-06 08:48:43 -0400

    fix: corrigir warning de configuração de autolinking
    - Remover podspecPath obsoleto do react-native.config.js
    - Simplificar configuração para autolinking moderno
    - Resolver warning 'podspecPath is not allowed'

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   dependency: {
     platforms: {
-      ios: {
-        podspecPath: "ios/RNReactNativeWalletPay.podspec",
-        configurations: ["Debug", "Release"],
-      },
       android: {
-        sourceDir: "android/src/main/java",
-        packageImportPath: "import com.reactlibrary.RNReactNativeWalletPayPackage;",
+        sourceDir: "../android/src/main/java/",
+        packageImportPath:
+          "import com.reactlibrary.RNReactNativeWalletPayPackage;",
       },
     },
   },
